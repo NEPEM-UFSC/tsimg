8e9844e6334b17fca59ff635da8963187613131a
Matheus L Machado <matheuslmachado03@gmail.com>
2024-12-26 19:23:03 -0300

    Atualizar informações de build e adicionar template HTML para exibição de imagens com controle deslizante

--- src/build_info.h
+++ src/build_info.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define BUILD_INFO "Version: 0.0.1\nMicroversion: alpha\nBuild Number: 291\nBuild Date: 2024-12-26 17:00:56"
+#define BUILD_INFO "Version: 0.0.1\nMicroversion: alpha\nBuild Number: 244\nBuild Date: 2024-12-26 19:10:49"
