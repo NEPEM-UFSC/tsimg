24e04e0bdbb947f2ec32af6be09d8cd02731f154
Matheus L Machado <matheuslmachado03@gmail.com>
2024-12-26 21:07:18 -0300

    Atualizar informações de versão para 0.1.1, incluindo número de build 248 e data de compilação.

--- src/build_info.h
+++ src/build_info.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define BUILD_INFO "Version: 0.1.1\nMicroversion: alpha\nBuild Number: 247\nBuild Date: 2024-12-26 21:00:36"
+#define BUILD_INFO "Version: 0.1.1\nMicroversion: alpha\nBuild Number: 248\nBuild Date: 2024-12-26 21:06:55"
