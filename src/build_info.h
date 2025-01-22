8a8cbed11a65df4eae401d499a86f3d50582f1dd
Matheus L Machado <matheuslmachado03@gmail.com>
2025-01-21 21:48:44 -0300

    Atualizar informações de versão para 0.1.4, incluindo novos números de versão e build.

--- src/build_info.h
+++ src/build_info.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define BUILD_INFO "Version: 0.1.3\nMicroversion: alpha\nBuild Number: 258\nBuild Date: 2025-01-02 15:16:08"
+#define BUILD_INFO "Version: 0.1.4\nMicroversion: alpha\nBuild Number: 259\nBuild Date: 2025-01-21 21:46:45"
