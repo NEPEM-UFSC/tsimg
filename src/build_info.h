f4305dfd4234fedf046ac269956d3230ecd2b5e1
Matheus L Machado <matheuslmachado03@gmail.com>
2025-01-21 23:08:18 -0300

    Atualizar informações de versão para 0.1.6.265, incrementando o número de build para 265.

--- src/build_info.h
+++ src/build_info.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define BUILD_INFO "Version: 0.1.6\nMicroversion: alpha\nBuild Number: 264\nBuild Date: 2025-01-21 22:51:11"
+#define BUILD_INFO "Version: 0.1.6\nMicroversion: alpha\nBuild Number: 265\nBuild Date: 2025-01-21 23:07:49"
