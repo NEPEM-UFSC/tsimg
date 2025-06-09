2a9121b408038f5dbce4dc9f34cb3f764cde6de2
Matheus L Machado <matheuslmachado03@gmail.com>
2025-06-09 20:01:50 -0300

    feat: Atualizar informações de build para a versão 0.1.7.272

--- src/build_info.h
+++ src/build_info.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define BUILD_INFO "Version: 0.1.7\nMicroversion: alpha\nBuild Number: 271\nBuild Date: 2025-01-22 18:40:21"
+#define BUILD_INFO "Version: 0.1.7\nMicroversion: alpha\nBuild Number: 272\nBuild Date: 2025-05-08 13:25:32"
